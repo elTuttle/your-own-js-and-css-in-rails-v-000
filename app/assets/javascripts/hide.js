5081196b9e8b46c176aef0791e1a56fb7360596c
John Tuttle <thejwt@gmail.com>
2017-12-15 08:14:45 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,5 @@
 function hideWhenClicked() {
   var hide_this = document.getElementById('hide_this');
-  var body = document.getElementByTagName('body');
+  var body = document.getElementsByTagName('body')[0];
   body.removeChild(hide_this);
 }
